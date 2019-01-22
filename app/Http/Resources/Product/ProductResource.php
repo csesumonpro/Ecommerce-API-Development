5a8845215ff422388f5443ffe0b7e122f8565c63
Sumon Sarker <csesumonpro@gmail.com>
2019-01-23 00:56:53 +0600

    Modify product details in ProductResource file

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,11 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'detail' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
             'discount' => $this->discount,
+            'totalPrice' => round($this->price - $this->price * $this->discount / 100, 2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count()) : '',
+            'href' => ['reviews' => route('reviews.index', $this->id)]
         ];
     }
 }
