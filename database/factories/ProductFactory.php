1b687c1285e6776338417cb1584b823d2f315507
Sumon Sarker <csesumonpro@gmail.com>
2019-01-22 21:25:56 +0600

    Product and Review Factory added

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween(20, 100),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2, 18),
     ];
 });
